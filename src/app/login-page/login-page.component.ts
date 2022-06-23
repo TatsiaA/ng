fe640515be4afb6d3bb95817c84eea444a7f2f38
TatsiaA <taniokk@mail.ru>
2022-06-23 21:15:55 +0300

    add login page

---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent {
+
+  constructor() { }
+
+  userName:string="Name";
+}
